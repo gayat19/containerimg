be64f59bdf36ad10ef6754c2048b60b512e1f83a
Gayathri Mahadevan <gayat_19@yahoo.com>
2021-03-30 11:46:54 +0530

    Initial Commit

---
+++ Models/MoviesRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BuildPipeEditDockerProject.Models
+{
+    public class MoviesRepository : IRepository<Movie>
+    {
+        public List<Movie> movies;
+
+        public MoviesRepository()
+        {
+            movies = new List<Movie>();
+            movies.Add(new Movie
+            {
+                Id = 101,
+                Name = "X-Men",
+                Duration = 123.2f
+            });
+        }
+
+        public void AddItem(Movie movie)
+        {
+            this.movies.Add(movie);
+        }
+
+        public IEnumerable<Movie> GetItems()
+        {
+            return this.movies;
+        }
+
+        public Movie GetItemById(int id)
+        {
+            return this.movies.FirstOrDefault(m => m.Id == id);
+        }
+
+         
+    }
+}
